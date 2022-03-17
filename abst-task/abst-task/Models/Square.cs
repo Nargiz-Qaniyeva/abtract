ad8672cd9d55cc04d022d74e64c76af291207b43
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-18 02:50:37 +0400

    first commit

---
+++ abst-task/abst-task/Models/Square.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abst_task.Models
+{
+    internal class Square
+    {
+    }
+}
---
+++ abst-task/abst-task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abst_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
